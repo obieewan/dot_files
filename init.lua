538a69a8455388c6096c662a9e1fe30192b1d9e8
Obie <obie.tizon@gmail.com>
2023-10-21 23:16:52 +0800

    Add packer plugin

--- init.lua
+++ init.lua
@@ -8,3 +8,13 @@ vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
 -- set relative number and number default upon opening nvim --
 vim.o.relativenumber = true
 vim.o.number = true
+
+-- use ,w and ,s for split windows --
+vim.api.nvim_set_keymap('n', '<leader>w', '<Cmd>vsplit<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>s', '<Cmd>split<CR>', { noremap = true })
+
+-- NERDTree mapping
+vim.api.nvim_set_keymap('n', '<leader>n', ':NERDTreeToggle<CR>', { noremap = true })
+
+-- this will run the packer.lua script
+require('packer_plugins')
