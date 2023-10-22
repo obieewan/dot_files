ed3695a900453cfa708fd8abac34ca471140c542
Obie <obie.tizon@gmail.com>
2023-10-23 01:52:10 +0800

    Add and show color swatches in autocomplete in tailwind

--- plugins.lua
+++ plugins.lua
@@ -22,6 +22,34 @@ local plugins ={
       require "custom.configs.lspconfig"
     end,
   }, 
+
+  {
+    "NvChad/nvim-colorizer.lua",
+    config = function ()
+      require("colorizer").setup({
+        user_default_options = {
+          tailwind = true,
+        },
+      })
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    },
+    opts = function(_, opts)
+      -- original LazyVim kind icon formatter
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        format_kinds(entry, item) -- add icons
+        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+      end
+    end,
+  },
+
+  -- Install by default LSP
   {
     "williamboman/mason.nvim",
     opts = {
