a0abae99963bfa2efcfffa3e8150f6c265f221fe
Obie <obie.tizon@gmail.com>
2023-10-23 01:46:09 +0800

    Updated lsp config

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,11 +1,9 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach()
-local capabalities = require("plugins.configs.lspconfig").capabilities
+local configs = require("plugins.configs.lspconfig")
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
 
-local lspconfig = require("lspconfig")
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
---[[
-local servers = { "html", "cssls", "clangd", "tailwindcss", "emmet_ls"}
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls", "clangd"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -13,11 +11,23 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
-]]
 
+-- Without the loop, you would have to manually set up each LSP 
+-- 
+-- lspconfig.html.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+--
+-- lspconfig.cssls.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+
+-- LSP Configuration for Tailwind with elixir phoenix
 lspconfig.tailwindcss.setup({
-  on_attach = on_attach,
   capabilities = capabilities,
+  on_attach = on_attach,
   filetypes = { "html", "elixir", "eelixir", "heex" },
   root_dir = lspconfig.util.root_pattern('tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js', 'postcss.config.ts', 'package.json', 'node_modules', '.git', 'mix.exs'),
   init_options = {
@@ -29,6 +39,17 @@ lspconfig.tailwindcss.setup({
   },
   settings = {
     tailwindCSS = {
+      classAttributes = { "class", "className", "class:list", "classList", "ngClass" },
+      lint = {
+        cssConflict = "warning",
+        invalidApply = "error",
+        invalidConfigPath = "error",
+        invalidScreen = "error",
+        invalidTailwindDirective = "error",
+        invalidVariant = "error",
+        recommendedVariantOrder = "warning"
+      },
+      validate = true,
       experimental = {
         classRegex = {
           'class[:]\\s*"([^"]*)"',
@@ -39,7 +60,7 @@ lspconfig.tailwindcss.setup({
 })
 
 lspconfig.emmet_ls.setup({
-  on_attach = on_attach,
   capabilities = capabilities,
+  on_attach = on_attach,
   filetypes = { "html", "css", "elixir", "eelixir", "heex" },
 })
