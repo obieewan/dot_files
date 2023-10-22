9e89b6a4320bf5261b0727ba4bff40202d0a7ffa
Obie <obie.tizon@gmail.com>
2023-10-22 18:23:25 +0800

    Add nvchad custom config

---
+++ chadrc.lua
@@ -0,0 +1,13 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = { 
+  theme = 'solarized_dark',
+  tabufline = {
+    enabled= false
+  }
+}
+
+M.plugins = "custom.plugins"
+
+return M
---
+++ init.lua
@@ -0,0 +1,32 @@
+-- alias ESC with jj and jk --
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true })
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
+
+-- use ,w and ,s for split windows --
+vim.api.nvim_set_keymap('n', ',w', '<Cmd>vsplit<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', ',s', '<Cmd>split<CR>', { noremap = true })
+
+-- set relative number and number default upon opening nvim --
+vim.o.relativenumber = true
+vim.o.numer = true
+
+-- Window resizing
+vim.api.nvim_set_keymap('n', '+', '<C-w>+', { noremap = true })
+vim.api.nvim_set_keymap('n', '-', '<C-w>-', { noremap = true })
+vim.api.nvim_set_keymap('n', '}', '<C-w>>', { noremap = true })
+vim.api.nvim_set_keymap('n', '{', '<C-w><lt>', { noremap = true })
+
+-- Easier window navigation
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
+
+vim.api.nvim_set_keymap('i', '<C-h>', '<C-w>h', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-j>', '<C-w>j', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-k>', '<C-w>k', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-l>', '<C-w>l', { noremap = true })
+
+
+-- Ruler
+vim.o.ruler = true
